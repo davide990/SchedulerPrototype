d8440c68b220732563de30e3d38285de6d71f7f0
Davide Andrea Guastella <davide-andrea.guastella@lip6.fr>
2017-03-28 11:47:01 +0200

    Added Executable interface to PlanImpl

--- src/org/lip6/scheduler/PlanImpl.java
+++ src/org/lip6/scheduler/PlanImpl.java
@@ -4,10 +4,20 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class PlanImpl implements Plan {
+public class PlanImpl implements Plan, Executable {
+
+	private final int ID;
 
 	final LinkedHashMap<Integer, Executable> tasks = new LinkedHashMap<>();
+
+	private final static Logger logger = Logger.getLogger(PlanImpl.class.getName());
+
+	public PlanImpl(int ID) {
+		this.ID = ID;
+	}
 
 	@Override
 	public void addTask(Task t) {
@@ -30,6 +40,15 @@ public class PlanImpl implements Plan {
 	public Map<Integer, Executable> tasks() {
 		return Collections.unmodifiableMap(tasks);
 
+	}
+
+	@Override
+	public void execute(String[] args) {
+		logger.log(Level.FINEST, "Executing plan [" + ID + "]");
+		tasks.forEach((k, v) -> {
+			logger.log(Level.FINEST, "Executing task [" + k + "]");
+			v.execute(args);
+		});
 	}
 
 }
