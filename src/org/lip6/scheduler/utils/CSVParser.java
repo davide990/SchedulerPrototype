a99fa4566385cf3bb6c880890720c5ff3eadc8d0
Davide Andrea Guastella <davide-andrea.guastella@lip6.fr>
2017-04-12 14:10:46 +0200

    Added method for serialize plans

--- src/org/lip6/scheduler/utils/CSVParser.java
+++ src/org/lip6/scheduler/utils/CSVParser.java
@@ -1,33 +1,38 @@
 package org.lip6.scheduler.utils;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
+import org.lip6.scheduler.Plan;
 import org.lip6.scheduler.PlanImpl;
+import org.lip6.scheduler.Task;
 import org.lip6.scheduler.TaskFactory;
 
 public class CSVParser {
 
-	public static void main(String[] args) {
-		Map<Integer, PlanImpl> p = null;
-		try {
-			p = parse("/home/davide/task_benchmark_papero.csv");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	/*
+	 * public static void main(String[] args) { Map<Integer, PlanImpl> p = null;
+	 * try { p = parse("/home/davide/task_benchmark_papero.csv"); } catch
+	 * (IOException e) { e.printStackTrace(); }
+	 * 
+	 * p.forEach((k, v) -> { System.out.println(v); });
+	 * 
+	 * // -----
+	 * 
+	 * try { serialize(new ArrayList(p.values()),
+	 * "/home/davide/task_benchmark_papero.csv.reverse"); } catch (IOException
+	 * e) { // TODO Auto-generated catch block e.printStackTrace(); } }
+	 */
 
-		p.forEach((k, v) -> {
-			System.out.println(v);
-		});
-
-	}
-
-	public enum headers {
+	private enum headers {
 		taskID, planID, planPriority, resourceID, releaseTime, processingTime
 	}
 
@@ -41,7 +46,7 @@ public class CSVParser {
 			int planID = Integer.parseInt(record.get("planID"));
 			int planPriority = Integer.parseInt(record.get("planPriority"));
 			plans.putIfAbsent(planID, PlanImpl.get(planID, planPriority));
-			
+
 			int taskID = Integer.parseInt(record.get("taskID"));
 			int resourceID = Integer.parseInt(record.get("resourceID"));
 			int releaseTime = Integer.parseInt(record.get("releaseTime"));
@@ -50,5 +55,22 @@ public class CSVParser {
 		}
 
 		return plans;
+	}
+
+	public static void serialize(List<Plan> plans, String fname) throws IOException {
+
+		final FileWriter fout = new FileWriter(fname);
+		final CSVPrinter printer = CSVFormat.EXCEL.withHeader(headers.class).print(fout);
+
+		for (Plan p : plans) {
+			for (Task t : p.tasks()) {
+				printer.printRecord(t.getTaskID(), t.getPlanID(), p.getPriority(), t.getResourceID(),
+						t.getReleaseTime(), t.getProcessingTime());
+			}
+
+		}
+		fout.flush();
+		fout.close();
+		printer.close();
 	}
 }
